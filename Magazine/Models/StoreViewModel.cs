0ef9d17c54c1375382e74e101278f638e019f776
Vladimir Makarevich <business.makarevich@outlook.com>
2017-04-15 00:49:20 +0300

    added view and changed css

--- Magazine/Models/StoreViewModel.cs
+++ Magazine/Models/StoreViewModel.cs
@@ -7,6 +7,7 @@ namespace Magazine.Models
 {
     public class StoreViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string StartWorkTime { get; set; }

21ce79256dca358c916303b1d708abaf24ba128f
Vladimir Makarevich <business.makarevich@outlook.com>
2017-04-15 20:20:43 +0300

    removed the comments and unusage model

--- Magazine/Scripts/Custom/GetItemByStore.js
+++ Magazine/Scripts/Custom/GetItemByStore.js
@@ -1,47 +1,4 @@
-﻿//$(document).ready(function () {
-
-//$("#GetListItems").click(function (e) {
-//    e.preventDefault();
-//    var data = new Object();
-//    var id = $("#Id").val();
-//    data.id = id;
-
-//    $.ajax({
-//        type: 'GET',
-//        contentType: 'application/json; charset=utf-8',
-//        url: '/Magazine/GetListItems',
-//        data: data,
-//        success: function (data) {
-//            for (var i = 0; i < data.length; i++) {
-//                tr = $("<tr/>");
-//                tr.append("<td>" + data[i].Name + "</td>");
-//                tr.append("<td>" + data[i].Description + "</td>");
-//                $("table.table-bordered.items").append(tr);
-//            }
-//        }
-//    });
-//});
-//};
-
-//function itemsTable(container) {
-
-//    function locate() {
-
-//        $("itemTable").hide();
-//    }
-
-//    return ({
-//        init: function () {
-
-//            locate();
-//        },
-//    })
-//};
-
-//var ItemsList = itemsTable(".manageProject");
-//ItemsList.init();
-
-function sentData(item) {
+﻿function sentData(item) {
 
     clearTable();
     removeHidden();
@@ -67,16 +24,18 @@ function clearTable() {
 };
 
 function removeHidden() {
+
     $(function () {
         $('#itemTable').removeClass('hidden');
     });
-}
+};
 
 function getTable(data) {
+
     for (var i = 0; i < data.length; i++) {
         tr = $("<tr/>");
         tr.append("<td>" + data[i].Name + "</td>");
         tr.append("<td>" + data[i].Description + "</td>");
         $("table.table-bordered.items").append(tr);
-    }
-}
+    };
+};

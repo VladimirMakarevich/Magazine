d11bc51b227b2a3135f56b8bb47769576cc39594
Vladimir Makarevich <business.makarevich@outlook.com>
2017-04-15 23:10:32 +0300

    deleted unused code

--- Magazine.Api/Controllers/BaseController.cs
+++ Magazine.Api/Controllers/BaseController.cs
@@ -1,10 +1,5 @@
 ﻿using Magazine.BL.UnityOfWork;
 using Magazine.Mappers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace Magazine.Api.Controllers

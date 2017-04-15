c7b11f4a45c0268275123df54e3fc07c9bfc8333
Vladimir Makarevich <business.makarevich@outlook.com>
2017-04-15 10:35:05 +0300

    added changes to js and view

--- Magazine/Scripts/Custom/GetItemByStore.js
+++ Magazine/Scripts/Custom/GetItemByStore.js
@@ -4,15 +4,14 @@
         var data = new Object();
         var id = $("#Id").val();
         data.id = id;
-        var json = JSON.stringify(data);
 
         $.ajax({
             type: 'GET',
             contentType: 'application/json; charset=utf-8',
             url: '/Magazine/GetListItems',
-            data: json,
+            data: data,
             success: function (data) {
-                $("#Login").val(data.result);
+                $("#result").val(data);
             }
         });
     });
